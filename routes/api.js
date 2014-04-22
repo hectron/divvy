fd787555c06708b8f960e8b3999addf634845fd0
SL-WestinWrzesinski <wwrzesinski@shoplocal.com>
2014-04-22 16:24:08 -0500

    removing node files

--- routes/api.js
+++
@@ -1,8 +0,0 @@
-/**
- * Reads data from database (currently static files)
- * Returns JSON
- */
-
-exports.getStations = function(req, res, next) {
-    res.sendfile('public/fixtures/Divvy_Stations_2013.json');
-};
