1bda26b9d6a51b362be9543d940a19a5796cd227
Hector Rios <labeledloser@gmail.com>
2014-04-27 17:33:30 -0500

    Included a gulpfile.js that is similar to our Gruntfile.js. Renamed the divvy files to be correct. Need to figure out if there are any missing dependencies in the gulp file.

--- app/server/server.js
+++ app/server/server.js
@@ -1,9 +1,9 @@
 var express = require('express'),
     routes = require('./routes/stations'),
-    diivy = require('./diivy/diivy');
+    divvy = require('./divvy/divvy');
 var app = express();
 
-diivy.requestJSON();
+divvy.requestJSON();
 
 app.get('/stations', routes.findAll);
 app.get('/stations/:id', routes.findById);
