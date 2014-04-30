eb5480ad9e1c52b5a504b5373c3453874c3665d1
Hector Rios <labeledloser@gmail.com>
2014-04-29 22:10:43 -0500

    Added the ability to find a station by location id.

--- app/server/routes/stations.js
+++ app/server/routes/stations.js
@@ -8,9 +8,33 @@ exports.findAll = function(req, res) {
 };
 
 exports.findById = function(req, res) {
-  res.send({id:req.params.id, name: "The Name", description: "description"});
+  var data = divvy.requestJSONPromise(),
+    targetId = req.params.id,
+    stations = [], station;
+
+  data.then(function (results) {
+    stations = cleanResults(results);
+
+    for (var i = 0, size = stations.length; !station && i < size; i++) {
+      if (targetId == stations[i].id) {
+        station = stations[i];
+      }
+    }
+
+    // check if the station is found
+    // otherwise send an error message
+    res.send(station);
+  });
+
+
 };
 
 exports.findByCoords = function(req, res){
   res.send({id:1, name:"The Name", description: "description"});
+};
+
+var cleanResults = function (divvyJson) {
+  divvyJson = divvyJson || {};
+  // the cache does not have the stationBeanList
+  return divvyJson.length ? divvyJson : divvyJson['stationBeanList'];
 };
