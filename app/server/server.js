803302040c7822e68bd2454b73630413bd556f70
Hector Rios <labeledloser@gmail.com>
2014-05-06 21:52:10 -0500

    Added error handling and started styling the page a bit.

--- app/server/server.js
+++ app/server/server.js
@@ -18,5 +18,27 @@ app.get('/', function(req, res){
 });
 
 app.use(express.static(public));
+
+/**
+ * Error handling
+ */
+
+// any url not configured, send 404
+app.get('*', function (req, res, next) {
+  var err = new Error();
+  err.status = 404;
+
+  next(err);
+});
+
+app.use(function (err, req, res, next) {
+  if (err.status !== 404)
+    return next();
+
+  var errorPath = path.resolve(public, '404.html');
+
+  res.sendfile(errorPath, err);
+});
+
 app.listen(8080);
 console.log('Listening on port 8080...');
